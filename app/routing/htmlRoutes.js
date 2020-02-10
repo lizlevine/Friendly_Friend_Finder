0931c0b80950e31d8a0a5fd379ca363a162ffe67
Tuscany <lizlevine.rigie@gmail.com>
2020-02-10 12:30:37 -0500

    chore:Fix local routing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,11 +10,20 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
+  // app.get("/survey", function(req, res) {
+  //   res.sendFile(path.join(__dirname + "/../public/survey.html"));
+  // });
+
+  // app.use(function(req, res) {
+  //   res.sendFile(path.join(__dirname + "/../public/home.html"));
+  // });
+
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname + "/../public/survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-  app.use(function(req, res) {
-    res.sendFile(path.join(__dirname + "/../public/home.html"));
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
