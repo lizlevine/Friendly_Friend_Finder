3a348a83875872746a80e7db4b03b52e013d51af
Tuscany <lizlevine.rigie@gmail.com>
2020-02-10 19:16:01 -0500

    MVP

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,50 +2,53 @@
 
 var friends = [
   {
-    "name": "Don Draper",
-    "photo":"https://upload.wikimedia.org/wikipedia/en/7/7b/Don_Draper_Wiki.jpg",
-    "scores": ["3", "1", "4", "1", "1", "3", "5", "3", "4", "5"]
+    name: "Don Draper",
+    photo: "https://upload.wikimedia.org/wikipedia/en/7/7b/Don_Draper_Wiki.jpg",
+    scores: ["5", "1", "5", "1", "1", "2", "5", "3", "1", "5"]
   },
   {
-    "name": "Phoebe Waller",
-    "photo":"https://snworksceo.imgix.net/dtc/1685c486-cf48-4328-a3bb-40319db45a55.sized-1000x1000.jpg?w=1000",
-      "scores": ["5", "4", "2", "1", "1", "3", "2", "3", "2", "1"]
+    name: "Phoebe Waller",
+    photo:
+      "https://snworksceo.imgix.net/dtc/1685c486-cf48-4328-a3bb-40319db45a55.sized-1000x1000.jpg?w=1000",
+    scores: ["5", "5", "2", "1", "1", "5", "1", "3", "1", "5"]
   },
   {
-    "name": "Bridgette Bird",
-    "photo":
+    name: "Bridgette Bird",
+    photo:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR4FDM57FwO9LmTYWwZEYTxmv2bpNo3-Ws8ngraA06P5fY9YwG8",
-      "scores": ["5", "4", "2", "1", "1", "3", "2", "3", "2", "1"]
+    scores: ["1", "4", "2", "1", "1", "3", "2", "3", "2", "1"]
   },
   {
-    "name": "Thomas A. Anderson",
-    "photo":
+    name: "Thomas A. Anderson",
+    photo:
       "https://i.gifer.com/origin/da/dac940f44768092fa3767205f994a00a_w200.gif",
-      "scores": ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+    scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "4"]
   },
   {
-    "name": "Elliot Alderson",
-    "photo": "https://psmag.com/.image/t_share/MTUxNzI3ODczNDU4MTg1NDAw/robot.jpg",
-      "scores": ["5", "4", "2", "1", "1", "3", "2", "3", "2", "1"]
+    name: "Elliot Alderson",
+    photo:
+      "https://psmag.com/.image/t_share/MTUxNzI3ODczNDU4MTg1NDAw/robot.jpg",
+    scores: ["5", "4", "2", "1", "1", "3", "2", "3", "2", "5"]
   },
   {
-    "name": "The Guy",
-    "photo": "https://tvguide1.cbsistatic.com/i/2015/04/20/c9d227d7-e011-4d1c-929a-f1f6854d2380/9ff8894a21eb1ef3476d8add46a1739a/150420-news-high-maintenance.jpg",
-      "scores": ["2", "4", "5", "4", "2", "4", "1", "3", "2", "5"]
+    name: "The Guy",
+    photo:
+      "https://tvguide1.cbsistatic.com/i/2015/04/20/c9d227d7-e011-4d1c-929a-f1f6854d2380/9ff8894a21eb1ef3476d8add46a1739a/150420-news-high-maintenance.jpg",
+    scores: ["2", "5", "5", "3", "1", "4", "1", "3", "1", "4"]
   },
 
   {
-    "name": "Brittany Diaz",
-    "photo":
+    name: "Brittany Diaz",
+    photo:
       "https://15xomi2v386wytrb8nbsoq34-wpengine.netdna-ssl.com/wp-content/uploads/2016/09/millenial-women-660x400.jpg",
-      "scores": ["2", "4", "5", "4", "2", "4", "1", "3", "2", "5"]
+    scores: ["2", "4", "5", "4", "2", "4", "1", "3", "2", "5"]
   },
 
   {
-    "name": "Midge Maisel",
-    "photo":
+    name: "Midge Maisel",
+    photo:
       "https://media2.fdncms.com/ntslo/imager/u/original/4714787/musicartsculture_movies2-1-30a15dd8db497241.jpg",
-      "scores": ["2", "4", "5", "4", "2", "4", "1", "3", "2", "5"]
+    scores: ["2", "4", "5", "2", "2", "2", "1", "2", "2", "5"]
   }
 ];
 
